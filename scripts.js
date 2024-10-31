a3468b89e497c13f70a04b75fcfa078789b15d1e
Thiago <Thiago.a.rostomashvili@gmail.com>
2024-10-31 17:36:04 +0000

    função de ligar o som.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-alert("eai deu bom");
+let botaoSom = document.querySelector(".botao-som");
+let video = document.querySelector(".video");
+
+botaoSom.addEventListener("click", ligaSom);
+
+function ligaSom(){
+    video.muted = false
+}
