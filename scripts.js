e52f961caba0a8bc5573f15000834f3b6925bd2f
Thiago <Thiago.a.rostomashvili@gmail.com>
2024-11-02 17:27:39 +0000

    Etapa do modal

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
 let botaoSom = document.querySelector(".botao-som");
 let video = document.querySelector(".video");
+let botao = document.querySelector(".container-info");
+let modal  = document.querySelector(".modal");
 
+// Ligar o Som
 botaoSom.addEventListener("click", ligaSom);
 
 function ligaSom(){
     video.muted = false
+};
+
+// Mostrar o modal e sair do modal
+botao.addEventListener("click", mostrarModal);
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal() {
+    modal.style.display = "block"
+};
+
+function esconderModal(){
+    modal.style.display = "none"
 }
